b61b3b8f7f49fd2f240170fa8d69136d899a3601
KevinSanchezO <kesanchezo@gmail.com>
2021-09-20 23:11:14 -0600

    Se hicieron pruebas para cargar datos de archivos y acceder a rutas

--- Proyecto.hs
+++ Proyecto.hs
@@ -1,3 +1,5 @@
+import System.IO
+
 menu::IO()
 menu=
     do
@@ -10,27 +12,28 @@ menu=
             _ -> errorDigito
            
 menuAdministrativo :: IO()
-
 menuAdministrativo= 
     do 
         putStrLn "\n*****Opciones Administrativas*****\n 1.Informacion de hotel\n 2.Cargar tipo de habitaciones\n 3.Asignar cantidad de habitaciones por tipo\n 4.Carga de tarifas\n 5.Consultar reservacion\n 6.Consulta de facturas\n 7.Estadisticas de ocupacion\n Digite una opcion: "
         s<-getLine
         case s of
             "1" ->infoHoteles
-            {-"2"->cargarTipoHabitacion
-            "3"->asignarCantidadHabitacionesTipo
+            "2"->cargarTipoHabitacion
+            {-"3"->asignarCantidadHabitacionesTipo
             "4"->cargarTarifas
             "5"->consultarReservacion
             "6"->consultaFacturas
             "7"->estadisticasOcupacion-}
             _ -> errorDigito
+
 errorDigito::IO()
 errorDigito=
     do
         putStr "\ESC[2J"
         putStrLn "Error!!, Digite una opcion valida"
         menu
-menuOpcionesGenerales:: IO()
+
+menuOpcionesGenerales::IO()
 menuOpcionesGenerales=
     do
         putStrLn "\n******Opciones Generales*****\n 1.Reservacion\n 2.Cancelar Reservacion\n 3.Facturar Reservacion\n 4.Salir\n Digite una opcion: "
@@ -42,9 +45,18 @@ menuOpcionesGenerales=
             "4"->menu-}
             _ ->errorDigito
 
-infoHoteles:: IO()
-infoHoteles=
+infoHoteles::IO()
+infoHoteles =
     do
-        putStrLn "hola mundo"
-{-square :: Num a => a ->a
-square a = a*a-}
+        putStrLn "\nInformacion del hotel\nNombre   Cedula Juridica   Sitio Web   Telefono   Pais   Provincia"
+        cs <- readFile "infoHoteles.txt"
+        putStrLn cs
+
+cargarTipoHabitacion:: IO()
+cargarTipoHabitacion =
+    do 
+        putStrLn "\nIngrese la ruta del archivo con los tipos de habitaciones: "
+        ruta <- getLine
+
+        cs <- readFile ruta
+        putStrLn cs
