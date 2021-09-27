084f5ea979b0349fb17101aa01b63f7ac49278a3
KevinSanchezO <kesanchezo@gmail.com>
2021-09-27 00:28:34 -0600

    Arreglos en la funcionalidad de Carga de tipo de habitacion

--- Proyecto.hs
+++ Proyecto.hs
@@ -1,12 +1,7 @@
 import System.IO
 import Control.DeepSeq
 import Control.Exception
-{-import Text.ParserCombinators.Parsec.Language (GenLanguageDef(reservedOpNames, reservedNames))
-import Data.Array (listArray)
-import GHC.ForeignPtr (finalizeForeignPtr)
-import System.Console.Haskeline (Interrupt(Interrupt))
-import GHC.Real (numerator)
-import Distribution.Simple.InstallDirs (defaultInstallDirs)-}
+
 
 
 {-----Menu de inicio----
@@ -32,8 +27,8 @@ menuAdministrativo=
         s<-getLine
         case s of
             "1" ->infoHoteles
-            {-"2"->cargarTipoHabitacion
-            "3"->asignarCantidadHabitacionesTipo
+            "2"->cargarTipoHabitacion
+            {-"3"->asignarCantidadHabitacionesTipo
             "4"->cargarTarifas
             "5"->consultarReservacion
             "6"->consultaFacturas
@@ -96,4 +91,99 @@ split :: Char -> String -> [String]
 split c xs = case break (==c) xs of 
     (ls, "") -> [ls]
     (ls, x:rs) -> ls : split c rs
+
+{-------------------------------------------------------CARGA DE TIPO DE HABITACION-------------------------------------------------------------------}
+
+{----cargarTipoHabitaciones----
+solicita al usuario la ruta del archivo con los datos de habitaciones que desea ingresar
+y administra el proceso de guardar dichos datos-}
+cargarTipoHabitacion::IO()
+cargarTipoHabitacion =
+    do 
+        putStrLn "\nIngrese la ruta del archivo con los tipos de habitaciones: "
+        ruta <- getLine
+        listaA <- leerArchivo "habitacionesCargadas.txt"
+
+        contenido <- readFile ruta
+        let listaB = split ',' contenido
+         
+        let listaC = listaNoRepetidos listaB listaA 0
+        resetearArchivo "habitacionesCargadas.txt" ""
+        escribirEnHabitaciones "habitacionesCargadas.txt" listaC
+        menuAdministrativo
+        
+{----leerArchivo----
+Permite cargar en una lista los datos de un archivo cuyos datos esten separados por
+saltos de linea haciendo cada salto de linea un dato dentro de la lista-}
+leerArchivo :: System.IO.FilePath -> IO [String]
+leerArchivo (archivo) = do
+    contents <- readFile archivo
+    evaluate (force contents)
+    let lineas = lines contents
+    return lineas;
+
+{----listaNoRepetidos----
+verifica que en el archivo donde persisten los datos de las habitaciones
+no existan nombre de habitaciones repetidos
+-}
+listaNoRepetidos :: [String] -> [String] -> Int ->[String]
+listaNoRepetidos [] res opc = res
+listaNoRepetidos list res opc = do
+    let listaCortada = tail list
+    let listaCortada1 = tail listaCortada
+    let listaCortada2 = tail listaCortada1
+    let listaCortada3 = tail listaCortada2
+    let hd = head list
+    let res2 = res ++ [hd]
+    let opc1 = opc+1
+    let opc2 = opc+3
+    (if ((mod opc  3) == 0) && estaLista hd res then
+         listaNoRepetidos listaCortada2 res opc2
+    else
+        listaNoRepetidos listaCortada res2 opc1)
+
+{----resetearArchivo----
+Permite dejar en blanco un archivo de texto para volver a escribir su informacion
+modificada-}
+resetearArchivo :: System.IO.FilePath -> String -> IO ()
+resetearArchivo archivo info = do
+    writeFile archivo (info)
+
+{----estaLista----
+verifica si en una lista se encuentra un valor en especifico-}
+estaLista :: String->[String]->Bool
+estaLista nombre [] = False
+estaLista nombre lista = do
+    let primerElemento = head lista
+    let listaCortada = tail lista
+    if primerElemento == nombre then
+        True
+    else
+        estaLista nombre listaCortada
+
+
+{----escribirEnHabitaciones----
+se encarga de administrar la escritura de los datos de habitaciones sin que
+se repita datos de habitaciones en el archivo de la persistencia de datos-}
+escribirEnHabitaciones :: System.IO.FilePath->[String]->IO ()
+escribirEnHabitaciones archivo [] = menuAdministrativo
+escribirEnHabitaciones archivo lista = do
+    let dato = head lista
+    let tl_1 = tail lista
+
+    let guardarMensaje = dato  ++ "\n"
+    appendFile archivo guardarMensaje
+    escribirEnHabitaciones archivo tl_1
+       
+{--------------------------------------------------------------------------------------------------------------------------}
+
+stringToInt texto = do
+        let valor = read texto::Integer
+        return valor
+        
+
+intToString numero = do
+        let valor = show numero
+        return valor
+
 
